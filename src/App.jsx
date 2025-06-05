09d954617b2fd82ce07a18e836184fa077439667
Your Nameakshitha <your.email@example.combolusaniakshitha150@gmail.com>
2025-06-04 19:00:21 -0700

    "Initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div claaName='p-5 border border-success '>
+   <h1>welcome to react js</h1>
+   <button className='btn btn-danger'> clickme </button>
+    <button className='btn btn-dark'> clickme </button>
+     <button className='btn btn-light'> clickme </button>
+      <button className='btn btn-success'> clickme </button>
+       <button className='btn btn-primary'> clickme </button>
+        <button className='btn btn-secondary'> clickme </button>
+
+   </div>
+        
+  )
+}
+
+export default App
